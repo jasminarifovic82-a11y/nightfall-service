8a635b6cb2d66c465ce64dbfca8d04a580c5324c
jasminarifovic82-a11y <jasminarifovic82@gmail.com>
2026-03-20 20:35:47 +0100

    Add files via upload

---
+++ product.js
@@ -0,0 +1,16 @@
+const PRODUCTS = [
+  {
+    id: "license1",
+    name: "Nightfall License",
+    desc: "Best retail bot license",
+    image: "https://via.placeholder.com/200",
+    gamepass: "https://www.roblox.com/game-pass/123456"
+  },
+  {
+    id: "license2",
+    name: "Premium License",
+    desc: "Extra features + priority support",
+    image: "https://via.placeholder.com/200",
+    gamepass: "https://www.roblox.com/game-pass/654321"
+  }
+];
