67eb0bb36181284684f78e2ac928988becde5ca4
jasminarifovic82-a11y <jasminarifovic82@gmail.com>
2026-03-28 18:44:12 +0100

    Add files via upload

--- login.js
+++ login.js
@@ -1,3 +1,6 @@
 export default function handler(req, res) {
-  res.status(200).send("Login endpoint working");
+  const redirect = `https://discord.com/oauth2/authorize?client_id=${process.env.CLIENT_ID}&response_type=code&redirect_uri=${process.env.REDIRECT_URI}&scope=identify`;
+
+  res.writeHead(302, { Location: redirect });
+  res.end();
 }
