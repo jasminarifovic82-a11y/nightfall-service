e006013b5c6f9120ca36056b2545a87e5b4b9910
jasminarifovic82-a11y <jasminarifovic82@gmail.com>
2026-03-28 19:20:17 +0100

    Delete login.js

--- login.js
+++
@@ -1,6 +0,0 @@
-export default function handler(req, res) {
-  const redirect = `https://discord.com/oauth2/authorize?client_id=${process.env.CLIENT_ID}&response_type=code&redirect_uri=${process.env.REDIRECT_URI}&scope=identify`;
-
-  res.writeHead(302, { Location: redirect });
-  res.end();
-}
